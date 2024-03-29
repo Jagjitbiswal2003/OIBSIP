980322b96dcfa68aadf0c946ad7f27741aff4077
Jagjit Biswal <157984815+Jagjitbiswal2003@users.noreply.github.com>
2024-03-30 00:45:13 +0530

    My_portfolio

--- script.js
+++ script.js
@@ -1,20 +1,73 @@
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            nav.classList.toggle('show')
-        })
-    }
-}
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
 
-showMenu('nav__toggle','nav-menu')
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
 
-/*GSAP ANIMATION*/
-gsap.from('.home__title', {opacity: -1, duration: 1.5, delay:1})
-gsap.from('.home__img', {opacity: 0, duration: 1, delay:1, x: 100})
-gsap.from('.home__information', {opacity: 0, duration: 1, delay:1.2, y: -30})
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
 
-gsap.from('.box__medium', {opacity: 0, duration: 1.5, delay:1.3, y: -50})
-gsap.from('.box__small', {opacity: -1, duration: 1.5, delay:1.4, y: -50})
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: [ "Frontend Developer!","Web Designer!"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Frontend Developer!","Web Designer!"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
